239f5dc5973a7a191ea83cea85fa597f8df60b7f
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2024-09-02 21:51:43 +0200

    feat: add base methods for categories module

--- src/management/categories/categories.controller.ts
+++ src/management/categories/categories.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ApiSecurity, ApiTags } from '@nestjs/swagger';
+import { CategoriesService } from './categories.service';
+import { CategoryDto, CreateCategoryDto } from './dto';
 
+// TODO: Add permission guard
 @ApiSecurity('bearerAuth')
 @ApiTags('management/categories')
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+  constructor(private readonly _categoriesService: CategoriesService) {}
+
+  @Get()
+  getCategories(): Promise<CategoryDto[]> {
+    return this._categoriesService.getCategories();
+  }
+
+  @Post()
+  createCategory(
+    @Body() createCategoryDto: CreateCategoryDto,
+  ): Promise<CategoryDto> {
+    return this._categoriesService.createCategory(createCategoryDto);
+  }
+}
--- src/management/categories/categories.service.ts
+++ src/management/categories/categories.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaService } from '@src/prisma/prisma.service';
+import { CategoryDto, CreateCategoryDto } from './dto';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  constructor(private readonly _prismaService: PrismaService) {}
+
+  async getCategories(): Promise<CategoryDto[]> {
+    const categories = await this._prismaService.category.findMany();
+    return categories.map((category) => new CategoryDto(category));
+  }
+
+  async createCategory(
+    createCategoryDto: CreateCategoryDto,
+  ): Promise<CategoryDto> {
+    console.log(createCategoryDto);
+    // TODO: Implement this method
+    try {
+      const newCategory = await this._prismaService.category.create({
+        data: createCategoryDto,
+      });
+      return new CategoryDto(newCategory);
+    } catch (err) {
+      if (err.code === 'P2002') {
+        throw new HttpException('Category already exists', HttpStatus.CONFLICT);
+      }
+      throw new HttpException(
+        'Internal server error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+}
