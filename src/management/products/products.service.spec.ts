8c6593e1ec9b638df81e334ac1754147a23fcb6f
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2024-09-08 19:57:12 +0200

    feat: add base ut for getProducts

--- src/management/products/products.service.spec.ts
+++ src/management/products/products.service.spec.ts
@@ -5,6 +5,16 @@ import { CategoriesService } from '../categories/categories.service';
 import { PermissionsGuard } from '../guards/permissions-guard';
 import { PrismaService } from '@src/prisma/prisma.service';
 import { prismaMock } from '@src/singleton';
+import { mockProducts } from './data/mock-products';
+import { ProductDto } from './dto';
+import { PaginationData } from '@src/common/models';
+
+function returnMockPaginationData(page: number, limit: number): ProductDto[] {
+  console.log('returnMockPaginationData start....', page, limit);
+  const start = (page - 1) * limit;
+  const end = start + limit;
+  return mockProducts.slice(start, end);
+}
 
 describe('ProductsService', () => {
   let productsService: ProductsService;
@@ -15,6 +25,14 @@ describe('ProductsService', () => {
         CategoriesService,
         ManagementPermissionsService,
         ProductsService,
+        // {
+        //   provide: ProductsService,
+        //   useValue: {
+        //     getProducts: jest.fn((page: number, limit: number) =>
+        //       returnMockPaginationData(page, limit),
+        //     ),
+        //   },
+        // },
         {
           provide: PermissionsGuard,
           useValue: {
@@ -33,5 +51,28 @@ describe('ProductsService', () => {
 
   it('productsService should be defined', () => {
     expect(productsService).toBeDefined();
+  });
+
+  describe('getProducts', () => {
+    it('should return an array of products', async () => {
+      const paginationData = new PaginationData<ProductDto>();
+      paginationData.getDataFromQuery({ page: '1', limit: '5' });
+
+      const mockCount = mockProducts.length;
+      const mockItems = returnMockPaginationData(
+        paginationData.page,
+        paginationData.limit,
+      );
+
+      prismaMock.$transaction.mockResolvedValueOnce([mockItems, mockCount]);
+
+      const result = await productsService.getProducts(paginationData);
+
+      expect(result.items.length).toBeLessThanOrEqual(paginationData.limit);
+      expect(result.totalRecords).toBe(mockCount);
+      expect(result.items[0]).toBeInstanceOf(ProductDto);
+      expect(result.page).toBe(paginationData.page);
+      expect(result.limit).toBe(paginationData.limit);
+    });
   });
 });
