863f31054a5874ab112d38ab4cfd0d79eca21660
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2024-11-18 21:05:26 +0100

    fix: hot fix for app start

--- src/management/permissions/management-permissions.module.ts
+++ src/management/permissions/management-permissions.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ManagementPermissionsService } from './management-permissions.service';
 import { ManagementPermissionsController } from './management-permissions.controller';
+import { PrismaService } from '@src/prisma/prisma.service';
 
 @Module({
   imports: [],
-  providers: [ManagementPermissionsService],
+  providers: [ManagementPermissionsService, PrismaService],
   controllers: [ManagementPermissionsController],
 })
 export class ManagementPermissionsModule {}
